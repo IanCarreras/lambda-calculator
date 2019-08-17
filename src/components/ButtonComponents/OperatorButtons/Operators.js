f04a5e7f0cdde19a18d0087e6d44d947830ca613
Ian <mus5002@aol.com>
2019-08-17 16:46:12 -0400

    exported data, imported data into appropriate files, added the data to state

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -3,14 +3,19 @@ import React from "react";
 //import any components needed
 
 //Import your array data to from the provided data file
+import {operators} from '../../../data.js'
+
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operators, useOperator] = useState(operators)
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operators}
     </div>
   );
 };
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,9 @@
 import React from "react";
+import Numbers from '../../components/ButtonComponents/NumberButtons/Numbers'
+
 
 const Display = () => {
   return <div>{/* Display any props data here */}</div>;
 };
+
+export default Display;
