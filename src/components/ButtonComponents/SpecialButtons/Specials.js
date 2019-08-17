515c065959c5d3a95ad8f8d07de30c6a181bb58a
Ian <mus5002@aol.com>
2019-08-17 18:06:26 -0400

    added special buttons

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,19 +1,22 @@
 import React, {useState} from "react";
 
 //import any components needed
+import SpecialButton from './SpecialButton'
 
 //Import your array data to from the provided data file
 import {specials} from '../../../data.js'
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-  const [specials, useSpecial] = useState(specials)
+  const [specialsState] = useState(specials)
   return (
-    <div>
+    <div className='specials-container'>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {specials}
+       {specialsState.map((special, indx) => {
+         return <SpecialButton key={indx} symbol={special} />
+       })}
     </div>
   );
 };
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,9 +1,8 @@
 import React from "react";
-import Numbers from '../../components/ButtonComponents/NumberButtons/Numbers'
 
 
-const Display = () => {
-  return <div className='display'>{/* Display any props data here */}</div>;
+const Display = (props) => {
+  return <div className='display'>{props.display}</div>;
 };
 
 export default Display;
